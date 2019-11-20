951b24d7ecdcb661fa5070220ace6b232f682701
Phil DeLong <philjdelong@gmail.com>
2019-11-20 14:56:57 -0700

    Clenup test

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe "shelter index page", type: :feature do
-  before :each do
-    @shelter_1 = Shop.create( name: "Shelter 1")
-    @shelter_2 = Shop.create( name: "Shelter 2")
+  it "can see all shops" do
+    shelter_1 = Shelter.create(name: "Shelter 1")
+    shelter_2 = Shelter.create(name: "Shelter 2")
 
     visit "/shelters"
-  end
 
-  it "can see all shops" do
-    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(shelter_1)
+    expect(page).to have_content(shelter_2)
   end
 end
