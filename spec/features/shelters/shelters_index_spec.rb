384b9ba49b50944bd8185ded3050b09835659258
Phil DeLong <philjdelong@gmail.com>
2019-11-20 16:32:04 -0700

    Update

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe "shelter index page", type: :feature do
   it "can see all shops" do
-    shelter_1 = Shelter.create(name: "Shelter 1")
-    shelter_2 = Shelter.create(name: "Shelter 2")
+    shelter_1 = Shelter.create
+    shelter_2 = Shelter.create
 
     visit "/shelters"
 
