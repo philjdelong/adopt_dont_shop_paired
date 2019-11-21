430f8755e52cbfe6192ef4f51677787eed8be597
Phil DeLong <philjdelong@gmail.com>
2019-11-21 08:42:57 -0700

    Include seeds without instance variable. Working on Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Shelter.destroy_all (this will destroy all instanced once seed is run)
+# Pet.destroy_all (this will destroy all instanced once seed is run)
+
+shelter_1 = Shelter.create(  name:     'The Best Shelter',
+                              address:  '1234 Address St.',
+                              city:     'Denver',
+                              state:    'CO',
+                              zip:      '80202'
+)
+shelter_2 = Shelter.create(  name:     'The Other Shelter',
+                              address:  '4321 Address St.',
+                              city:     'UnDenver',
+                              state:    'NotCO',
+                              zip:      '20208'
+)
--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -2,27 +2,31 @@ require 'rails_helper'
 
 describe "shelter index page", type: :feature do
     before :each do
-    @shelter_1 = Shelter.create(  name:     'The Best Shelter'
-                                  # address:  '1234 Address St.',
-                                  # city:     'Denver',
-                                  # state:    'CO',
-                                  # zip:      '80202'
+    @shelter_1 = Shelter.create(  name:     'The Best Shelter',
+                                  address:  '1234 Address St.',
+                                  city:     'Denver',
+                                  state:    'CO',
+                                  zip:      '80202'
     )
-    @shelter_2 = Shelter.create(  name:     'The Other Shelter'
-                                  # address:  '4321 Address St.',
-                                  # city:     'UnDenver',
-                                  # state:    'NotCO',
-                                  # zip:      '20208'
+    @shelter_2 = Shelter.create(  name:     'The Other Shelter',
+                                  address:  '4321 Address St.',
+                                  city:     'UnDenver',
+                                  state:    'NotCO',
+                                  zip:      '20208'
     )
 
     visit "/shelters"
   end
 
-  it "can see shelter 1" do
+  it "can see shelter 1 name" do
     expect(page).to have_content(@shelter_1.name)
+    # expect(page).to have_content(@shelter_1.address)
+    # expect(page).to have_content(@shelter_1.city)
+    # expect(page).to have_content(@shelter_1.state)
+    # expect(page).to have_content(@shelter_1.zip)
   end
 
-  it "can see shelter 2" do
+  it "can see shelter 2 name" do
     expect(page).to have_content(@shelter_2.name)
   end
 end
