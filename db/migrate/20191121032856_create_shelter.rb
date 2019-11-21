d82c934b8c5539d90b55922809b28e7364c49f3d
Phil DeLong <philjdelong@gmail.com>
2019-11-20 20:29:20 -0700

    Update active record

--- db/migrate/20191121032856_create_shelter.rb
+++ db/migrate/20191121032856_create_shelter.rb
@@ -2,6 +2,12 @@ class CreateShelter < ActiveRecord::Migration[5.1]
   def change
     create_table :shelters do |t|
       t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.timestamps
     end
   end
 end
