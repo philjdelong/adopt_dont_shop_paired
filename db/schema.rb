433bc59944d3355c7c07ac71bcb4359062f83c4f
Phil DeLong <philjdelong@gmail.com>
2019-11-20 20:37:58 -0700

    Update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121012258) do
+ActiveRecord::Schema.define(version: 20191121033701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
