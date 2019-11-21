4d808bd2f062d7258b9b7fd2c0710dcc667249d9
Phil DeLong <philjdelong@gmail.com>
2019-11-20 17:05:20 -0700

    Update shelters to have name param and pass index test

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120233423) do
+ActiveRecord::Schema.define(version: 20191121000103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shelters", force: :cascade do |t|
+    t.string "name"
   end
 
 end
--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 describe "shelter index page", type: :feature do
-  it "can see all shops" do
-    shelter_1 = Shelter.create
-    shelter_2 = Shelter.create
-
+    before :each do
+    @shelter_1 = Shelter.create(name: 'Shelter 1')
+    @shelter_2 = Shelter.create(name: 'Shelter 2')
     visit "/shelters"
+  end
 
-    expect(page).to have_content(shelter_1)
-    expect(page).to have_content(shelter_2)
+  it "can see all shops" do
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
   end
 end
