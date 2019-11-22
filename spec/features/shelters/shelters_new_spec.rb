576a7476b3d0bbe1472e5154d3768ae6745c9189
Phil DeLong <philjdelong@gmail.com>
2019-11-22 08:37:30 -0700

    Write fields for New Shelter creation

--- spec/features/shelters/shelters_new_spec.rb
+++ spec/features/shelters/shelters_new_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe "As a user" do
   describe "when i visit shelters new" do
     describe "and fill out the new shelter form", type: :feature do
-      
+
       it "i see a form for new shelter" do
         visit '/shelters'
 
         click_link "New Shelter"
         expect(current_path).to eq('/shelters/new')
 
-        fill_in 'Shelter Name', with: "Phil's Spot"
-        fill_in 'Shelter Address', with: "1234 Address"
+        fill_in 'Name', with: "Phil's Spot"
+        fill_in 'Address', with: "1234 Address"
         fill_in 'City', with: "City"
         fill_in 'State', with: "State"
+        fill_in 'Zip', with: "Zip"
 
-        click_button 'Submit'
+        click_button 'Create'
         expect(current_path).to eq('/shelters')
 
         expect(page).to have_content("Phil's Spot")
