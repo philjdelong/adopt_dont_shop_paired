ceac13e0496884590f997f6a5ffcb0b2bb50eb61
Phil DeLong <philjdelong@gmail.com>
2019-11-23 14:54:20 -0700

    Add pets to index

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+  get '/pets', to: 'pets#index'
 end
