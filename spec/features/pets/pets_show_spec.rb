806aaa74ee57724041b91e52f1cbcb7540c43f54
Phil DeLong <philjdelong@gmail.com>
2019-11-24 12:53:21 -0700

    Write spec for user story 9

--- spec/features/pets/pets_show_spec.rb
+++ spec/features/pets/pets_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As a user" do
-  describe "when i visit a shelters show pets", type: :feature do
+  describe "when i visit a pets show page", type: :feature do
     before :each do
       @phils_shelter = Shelter.create(  name:     "Phil's Shelter",
                                         address:  "1234 Address St.",
@@ -27,21 +27,31 @@ RSpec.describe "As a user" do
                               sex:          "Male",
                               shelter_id:   @courtneys_shelter.id
       )
-      visit "/shelters/#{@phils_shelter.id}/pets"
     end
 
-    it "i see all pets that belong to that shelter" do
+    it "i can see the pets info" do
+      visit "/pets/#{@fluffy.id}"
+
       expect(page).to have_css("img[src*='#{@fluffy.image}']")
       expect(page).to have_content("Fluffy")
+      expect(page).to have_content("Fluffy is very aggressive, but you'll love her!")
       expect(page).to have_content(12)
       expect(page).to have_content("Female")
-    end
-
-    it "i dont see pets from other shelters do" do
-      expect(page).to_not have_css("img[src*='#{@charles.image}']")
+      expect(page).to have_content("Adoptable")
+      
       expect(page).to_not have_content("Charles")
-      expect(page).to_not have_content(5)
-      expect(page).to_not have_content("Male")
     end
   end
 end
+
+    # User Story 9, Pet Show
+    #
+    # As a visitor
+    # When I visit '/pets/:id'
+    # Then I see the pet with that id including the pet's:
+    # - image
+    # - name
+    # - description
+    # - approximate age
+    # - sex
+    # - adoptable/pending adoption status
