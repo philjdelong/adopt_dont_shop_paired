19ae9fd4fdbf5c954e6dd10771cb7df2af44fe4a
Phil DeLong <philjdelong@gmail.com>
2019-11-25 12:22:19 -0700

    Update spec to unclude edit shelters

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -4,33 +4,27 @@ RSpec.describe "As the user" do
   describe "when i visit shelter index", type: :feature do
     before :each do
 
-      @shelter_1 = Shelter.create(  name:     'The Best Shelter',
+      @phils_shelter = Shelter.create(  name:     'The Best Shelter',
                                     address:  '1234 Address St.',
                                     city:     'Denver',
                                     state:    'CO',
                                     zip:      '80202'
                                   )
-
-      @shelter_2 = Shelter.create(  name:     'The Other Shelter',
-                                    address:  '4321 Address St.',
-                                    city:     'UnDenver',
-                                    state:    'NotCO',
-                                    zip:      '20208'
-                                  )
-
       visit "/shelters"
     end
 
     it "i can see shelter 1 name" do
-      expect(page).to have_content(@shelter_1.name)
-    end
-
-    it "i can see shelter 2 name" do
-      expect(page).to have_content(@shelter_2.name)
+      expect(page).to have_content(@phils_shelter.name)
     end
 
     it "i can click new shelter button" do
-      expect(page).to have_link('New Shelter')
+      click_on 'New Shelter'
+      expect(current_path).to eq('/shelters/new')
+    end
+
+    it "i can edit each shelter" do
+      click_on 'Edit Shelter'
+      expect(current_path).to eq("/shelters/#{@phils_shelter.id}/edit")
     end
   end
 end
