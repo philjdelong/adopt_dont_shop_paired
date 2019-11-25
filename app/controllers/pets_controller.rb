91f910a0f4bb28dce0744e001e9fa6d40a0e189b
Phil DeLong <philjdelong@gmail.com>
2019-11-25 12:04:33 -0700

    Merge

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,20 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  private
+
+    def pet_params
+      params.permit(:image, :name, :description, :age, :sex)
+    end
 end
