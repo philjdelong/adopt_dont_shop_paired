36bbe91b7dac24438ccfad0a5a8a101a73c2c806
Phil DeLong <philjdelong@gmail.com>
2019-11-24 19:58:08 -0700

    Create new pet view and update controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
   get '/', to: 'home#index'
-  get '/shelters', to: 'shelters#index'
   get '/pets/', to: 'pets#index'
-  get '/shelters/new', to: 'shelters#new'
-  get '/shelters/:id', to: 'shelters#show'
   get '/pets/:id', to: 'pets#show'
+  get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
+  get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-  get '/shelters/:id/pets', to: 'shelters#pets_index'
-  get '/shelters/:id/pets/new', to: 'shelters#pets_new'
+
+  get '/shelters/:shelter_id/pets', to: 'shelters#pets_index'
+  get '/shelters/:shelter_id/pets/new', to: 'shelters#pets_new'
+  post 'shelters/:shelter_id/pets', to: 'shelters#pets_create'
 end
