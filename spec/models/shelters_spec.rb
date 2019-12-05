75e6fb49681225a235624daf39301bffe889511d
Phil DeLong <philjdelong@gmail.com>
2019-12-05 14:22:24 -0700

    Write spec for shelter validations and relationships

---
+++ spec/models/shelters_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Shelter, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+  end
+
+  describe "relationships" do
+    it {should have_many :pets}
+    it {should have_many :reviews}
+  end
+end
