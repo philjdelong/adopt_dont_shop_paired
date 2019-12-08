e96d520244cb64ff7d0e35732d055524d3a4c30b
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-12-08 01:38:11 -0700

    Add no favorite pets functionality in index view

--- spec/features/favorites/error_message_without_favorited_pets_spec.rb
+++ spec/features/favorites/error_message_without_favorited_pets_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'As a visitor', type: :feature do
 
     it 'I see text saying that there are no favorited pets added' do
       visit '/favorites'
-
       expect(page).to have_content("You have not favorited any pets. Find a pet and click to add them to your favorites to see them here!")
     end
   end
