beeed1e4d882b7b36a79c1c0318c5a56ba85e411
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-12-08 00:50:16 -0700

    Add test file for user story 14

---
+++ spec/features/favorites/error_message_without_favorited_pets_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  describe 'Show error message when visiting favorites index without add any favorites' do
+
+    it 'I see text saying that there are no favorited pets added' do
+      visit '/favorites'
+
+      expect(page).to have_content("You have not favorited any pets. Find a pet and click to add them to your favorites to see them here!")
+    end
+  end
+end
