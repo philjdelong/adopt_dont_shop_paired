096e240a7962d7862f3f989b16d602e8154710c8
Phil DeLong <philjdelong@gmail.com>
2019-12-10 16:38:03 -0700

    Debug testing. All passing

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  has_many :pets_applications
+  has_many :pets_applications, dependent: :destroy
   has_many :applications, through: :pets_applications
 
   validates_presence_of :image,
